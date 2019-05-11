eec3684dd5aba2aa920feb72191558c1b1674bd3
Alejandro Zamora Fonseca <ale2014.zamora@gmail.com>
2019-05-11 17:30:37 -0300

    - Adding hard-coded originate function

--- dingodialer.lisp
+++ dingodialer.lisp
@@ -2,15 +2,26 @@
 
 (in-package #:dingodialer)
 
-(setf login-parameters '(("action" . "login")
-                         ("username" . "omnisup")
-                         ("secret" . "Sup3rst1c10n")))
-(setf base-url "http://172.18.0.2:7088/mxml")
+(setf *login-parameters* '(("action" . "login")
+                           ("username" . "omnileadsami")
+                           ("secret" . "5_MeO_DMT")))
+(setf *base-url* "http://172.46.0.2:8088/asterisk/mxml")
 
-(setf command-parameters '(("action" . "command")
-                           ("command" . "database show")))
+(setf *command-parameters* '(("action" . "command")
+                             ("command" . "database show")))
 
 (defun asterisk-db-show ()
   (let ((cookie-jar (make-instance 'drakma:cookie-jar)))
-    (drakma:http-request base-url :method :post :parameters login-parameters :cookie-jar cookie-jar)
-    (drakma:http-request base-url :method :post :parameters command-parameters :cookie-jar cookie-jar)))
+    (drakma:http-request *base-url* :method :post :parameters *login-parameters* :cookie-jar cookie-jar)
+    (drakma:http-request *base-url* :method :post :parameters *command-parameters* :cookie-jar cookie-jar)))
+
+
+(defun asterisk-originate ()
+  (let* ((cookie-jar (make-instance 'drakma:cookie-jar))
+         (*command-parameters* '(("action" . "originate")
+                                 ("exten" . "s")
+                                 ("channel" . "Local/21123132@from-dialer/n")
+                                 ("context" . "call-answered")
+                                 ("priority" . "1"))))
+    (drakma:http-request *base-url* :method :post :parameters *login-parameters* :cookie-jar cookie-jar)
+    (drakma:http-request *base-url* :method :post :parameters *command-parameters* :cookie-jar cookie-jar)))
