68fb40db0bc77ed01dacfbfb97c748a94ff76e59
Alejandro Zamora Fonseca <ale2014.zamora@gmail.com>
2019-06-18 01:28:07 -0300

    more little code

--- experiments.lisp
+++ experiments.lisp
@@ -1,3 +1,9 @@
+(ql:quickload :safe-queue :bordeaux-threads)
+
+
+(defconstant +eol+ (format nil "~a~a" #\return #\newline))
+
+
 (defclass message ()
   ((response :initarg :response)
    (data :initarg :data
@@ -39,5 +45,41 @@
   ;; a litle more code pending
 
   ;; has_header, get_header, __getitem__, __repr__, get_action
+
+  )
+
+
+(defclass manager ()
+  ((sock :accessor manager->sock :initform nil)
+   (title :accessor manager->title :initform nil)
+   (connected :accessor manager->connected)
+   (running :accessor manager->running)
+   (hostname :accessor manager->hostname)
+   (message-queue :accessor manager->message-queue)
+   (response-queue :accessor manager->response-queue)
+   (event-queue :accessor manager->event-queue)
+   (event-callbacks :accessor manager->event-callbacks
+                    :initform (make-hash-table))
+   (reswaiting :accessor manager->reswaiting
+               :initform '())
+   (seqlock :accessor manager->seqlock)
+   (seq :accessor manager->seq :initform 0)
+   (message-thread :accessor manager->message-thread)
+   (event-dispatch-thread :accessor manager->event-dispatch-thread)))
+
+(defmethod initialize-instance :after ((obj manager) &key)
+  ;; self._connected = threading.Event()
+  ;; self._running = threading.Event()
+  (setf (manager->hostname manager) (machine-instance))
+  (setf (manager->message-queue manager) (safe-queue:make-queue))
+  (setf (manager->response-queue manager) (safe-queue:make-queue))
+  (setf (manager->event-queue manager) (safe-queue:make-queue))
+
+  (setf (manager->seqlock manager) (bt:make-lock))
+
+  ;; self.message_thread = threading.Thread(target=self.message_loop)
+  ;; self.event_dispatch_thread = threading.Thread(target=self.event_dispatch)
+  ;; self.message_thread.setDaemon(True)
+  ;; self.event_dispatch_thread.setDaemon(True)
 
   )
